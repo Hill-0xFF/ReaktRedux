790cb8f48f1d0f5fa664ce7408f8ab7e942f4bdc
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-06-27 18:39:08 -0300

    [dev]: Chapter 01 - Added two more actions, using state to pass as a payload to an action argument.

--- chapter01-redux-basics/src/features/counter/Counter.tsx
+++ chapter01-redux-basics/src/features/counter/Counter.tsx
@@ -1,23 +1,30 @@
 // import { AnyAction, Reducer, Slice } from "@reduxjs/toolkit";
 // import { WritableDraft } from 'immer/dist/internal';
 // import { Slice, SliceCaseReducers } from "@reduxjs/toolkit";
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount,
+  decrementByAmount,
+} from './counterSlice';
 
 import '../../css/style.css';
 
 // type T_CounterSlice = {
-  // count: number;
-  // increment: (state: WritableDraft<{ count: number }>) => void;
-  // decrement: (state: WritableDraft<{ count: number }>) => void;
-  // counter: Pick<Slice, Name>;
+// count: number;
+// increment: (state: WritableDraft<{ count: number }>) => void;
+// decrement: (state: WritableDraft<{ count: number }>) => void;
+// counter: Pick<Slice, Name>;
 //   state: Slice<{ count: number; }, { increment: (state: WritableDraft<{ count: number; }>) => void; decrement: (state: WritableDraft<{ count: number; }>) => void; }, "counter">;reducer: Reducer<{
 //     count: number;
 // }, AnyAction>;
 //  Slice<{ count: number; }, { increment: (state: WritableDraft<{ count: number; }>) => void; decrement: (state: WritableDraft<{ count: number; }>) => void; }, "counter">.reducer: Reducer<{
-  // count: number;
+// count: number;
 // }, AnyAction>
-  //   // CreateSliceOptions<{ count: number; }, { increment: (state: WritableDraft<{ count: number; }>) => void; decrement: (state: WritableDraft<{ count: number; }>) => void; },
+//   // CreateSliceOptions<{ count: number; }, { increment: (state: WritableDraft<{ count: number; }>) => void; decrement: (state: WritableDraft<{ count: number; }>) => void; },
 // };
 
 const Counter = () => {
@@ -25,16 +32,49 @@ const Counter = () => {
   // const count = useSelector((state: Pick<Slice, Name>) => state.counter.count);
   const count = useSelector((state: any) => state.counter.count);
   const dispatch = useDispatch();
+  const [amount, setAmount] = useState<number | string>(0);
+
+  const handleInput = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(0);
+    console.log(typeof evt.currentTarget.value);
+    if (evt.currentTarget.value) setAmount(Number(evt.currentTarget.value));
+    return;
+  };
 
   return (
     <section className="counter__section">
       <p>Counter state: {count ? count : 0}</p>
+      <div className="counter__input">
+        <label htmlFor="amount__input">Amount</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount__input"
+          onChange={(evt) => handleInput(evt)}
+          value={amount}
+        />
+      </div>
       <div className="counter__button">
         <button className="button" onClick={() => dispatch(increment())}>
           +
         </button>
         <button className="button" onClick={() => dispatch(decrement())}>
           -
+        </button>
+        <button className="button" onClick={() => dispatch(reset())}>
+          Reset
+        </button>
+        <button
+          className="button"
+          onClick={() => dispatch(incrementByAmount(amount))}
+        >
+          Increment By Amount
+        </button>
+        <button
+          className="button"
+          onClick={() => dispatch(decrementByAmount(amount))}
+        >
+          Decrement By Amount
         </button>
       </div>
     </section>
