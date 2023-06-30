995067d5f3432b3966ebe53b965dd0015180f831
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-06-30 02:59:40 -0300

    [feat]: Chapter 03 - change initial state types.

--- chapter03-async-logic/src/features/posts/postsSlice.ts
+++ chapter03-async-logic/src/features/posts/postsSlice.ts
@@ -1,5 +1,5 @@
-import { Action, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { sub } from 'date-fns';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import { sub } from 'date-fns';
 
 export type TypePosts = {
   id: string;
@@ -20,34 +20,17 @@ type PostsState = {
   posts: TypePosts[];
 };
 
-const initialState = [
-  {
-    id: '1',
-    title: 'Learning Redux Toolkit',
-    content: 'Always good to learn new stuff.',
-    datetime: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    },
-  },
-  {
-    id: '2',
-    title: 'Using slices',
-    content: `Slices are pieces of the state managed by Redux, and here, is where you implement the initial state and actions `,
-    datetime: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    },
-  },
-];
+type InitialStatePosts = {
+  posts: TypePosts[],
+  status: string,
+  error: boolean | null,
+}
+
+const initialState: InitialStatePosts = {
+  posts: [],
+  status: 'idle', // idle | loading | succeeded | failed
+  error: null,
+};
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -57,7 +40,7 @@ const postsSlice = createSlice({
       reducer: (state, action: PayloadAction<TypePosts>) => {
         // fixed using
         // https://redux-toolkit.js.org/api/createSlice#customizing-generated-action-creators
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(id, title, content, userId, datetime) {
         return {
@@ -82,7 +65,7 @@ const postsSlice = createSlice({
     //incrementReaction: (state, action: PayloadAction<TypePosts, 'id'>) => {
     incrementReaction: (state, action) => {
       const { postId, reaction } = action.payload;
-      const existPost = state.find(post => post.id === postId)
+      const existPost = state.posts.find(post => post === postId)
       if(existPost) {
         existPost.reactions[reaction] += 1
         
